d78b137bacaa7fc1191abac5499adeaf62eda06e
bing <bingdai9@gmail.com>
2022-03-11 15:47:02 -0800

    wrote a test for the Confirm Password field

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,13 +6,35 @@ function App() {
     <div className="container my-5">
       <form>
         <div className="mb-3">
-          <label htmlFor="email" className="form">
+          <label htmlFor="email" className="form-label">
             Email address
           </label>
           <input
             type="email"
             id="email"
             name="email"
+            className="form-control"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirm-password"
             className="form-control"
           ></input>
         </div>
--- my-react-app/src/__test__/App.test.js
+++ my-react-app/src/__test__/App.test.js
@@ -4,5 +4,11 @@ import App from "../App";
 test("inputs should be initially empty", () => {
   render(<App />);
   const emailInputElement = screen.getByRole("textbox");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
   expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
 });
