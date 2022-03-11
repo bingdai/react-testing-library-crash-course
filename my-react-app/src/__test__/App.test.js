000884bdcd554ad517b1dc6a0c2004b6a30340a9
bing <bingdai9@gmail.com>
2022-03-11 15:18:24 -0800

    wrote a textbox being empty test

---
+++ my-react-app/src/__test__/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+test("inputs should be initially empty", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox");
+  expect(emailInputElement.value).toBe("");
+});
