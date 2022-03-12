b6057991c02f59c9eddc5217d5283cb5cba69d26
bing <bingdai9@gmail.com>
2022-03-11 16:09:35 -0800

    added tests to make sure the fields can be typed into

--- my-react-app/src/__test__/App.test.js
+++ my-react-app/src/__test__/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 test("inputs should be initially empty", () => {
@@ -11,4 +12,28 @@ test("inputs should be initially empty", () => {
   expect(emailInputElement.value).toBe("");
   expect(passwordInputElement.value).toBe("");
   expect(confirmPasswordInputElement.value).toBe("");
+});
+
+test("should be able to type an email", () => {
+  render(<App />);
+  const emailInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  userEvent.type(emailInputElement, "try@gmail.com");
+  expect(emailInputElement.value).toBe("try@gmail.com");
+});
+
+test("should be able to type into the Passsword field", () => {
+  render(<App />);
+  const passwordInputElement = screen.getByLabelText("Password");
+  userEvent.type(passwordInputElement, "testPassword123");
+  expect(passwordInputElement.value).toBe("testPassword123");
+});
+
+test("should be able to type into the confirm Passsword field", () => {
+  render(<App />);
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  userEvent.type(confirmPasswordInputElement, "confirmtestPassword123");
+  expect(confirmPasswordInputElement.value).toBe("confirmtestPassword123");
 });
